4302f2cec339e73efd151f4ba6a9af3b0858cc66
Filipe Nunez <filipebnunez@gmail.com>
2023-10-15 20:53:53 -0300

    fix: ajustes para rodar em container
    ref #1

--- app/core/projects.py
+++ app/core/projects.py
@@ -2,7 +2,7 @@ from gitlab.v4.objects import Project
 from app.core.gitlab import gl
 
 
-def all_projects(member: bool = True) -> list[Project]:
+def all_projects(member: bool = True):
     return gl.projects.list(member=member)
 
 
