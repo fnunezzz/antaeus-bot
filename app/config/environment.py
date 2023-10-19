e6dfec1b49ccd48b937f406c7ad3c0ed05d9c37e
Filipe Nunez <filipebnunez@gmail.com>
2023-10-18 22:39:30 -0300

    feat: inicio da implementacao de um classe de parser de configuracao
    ref #9

--- app/config/environment.py
+++ app/config/environment.py
@@ -1,10 +1,14 @@
 import os
+import yaml
 from dotenv import load_dotenv
 load_dotenv()
 
+PATH = os.environ['CONFIG_PATH']
+
+with open(PATH, 'r') as file:
+    CONFIG = yaml.safe_load(file)
+
+print(CONFIG)
+
 TOKEN = os.environ['TOKEN']
 GITLAB_URL = os.environ['GITLAB_URL']
-OLD_ISSUE_TIME_DELTA = int(os.environ.get('OLD_ISSUE_TIME_DELTA', 10))
-OLD_ISSUE_OLD_LABELS = os.environ.get('OLD_ISSUE_OLD_LABELS', '').split(',')
-OLD_ISSUE_NEW_LABELS = os.environ.get('OLD_ISSUE_NEW_LABELS', '').split(',')
-LABELS = os.environ.get('LABELS', '').split(',')
